740c89c5986815096d35abd4b18301fcdc7a6a1e
rockstarbryant <brianouma9124@gmail.com>
2026-01-17 20:27:24 +0300

    upt

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use('/api', auditMiddleware);
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 }));
