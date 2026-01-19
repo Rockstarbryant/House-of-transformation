1b12264db997f719a8f88f55e0429c44acda09da
rockstarbryant <brianouma9124@gmail.com>
2026-01-19 23:58:36 +0300

    upt

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ console.log('');
 
 const auditMiddleware = require('./middleware/auditMiddleware');
 const { protect } = require('./middleware/supabaseAuth');
-const maintenanceMiddleware = require('../backup/maintenanceMiddleware');
+//const maintenanceMiddleware = require('../backup/maintenanceMiddleware');
 
 connectDB();
 
